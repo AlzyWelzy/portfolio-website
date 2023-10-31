ddbf62c608779a6396f341349cdff6f686894b63
AlzyWelzy <welzyalzy@gmail.com>
2023-11-01 02:10:48 +0530

    adding values

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "manvendra.raj.2002@gmail.com",
+      to: "manvendra@alzywelzy.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
