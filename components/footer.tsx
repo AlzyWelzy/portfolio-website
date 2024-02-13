53f8270649ce9d77035a530728784a49f17e40fb
AlzyWelzy <welzyalzy@gmail.com>
2024-02-13 18:52:57 +0530

    .

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,12 +4,13 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
+            <small className="mb-2 block text-xs">
+        &copy; {currentYear} Manvendra Rajpoot. All rights reserved.
+      </small>
       <p className="text-xs">
         <span className="font-semibold">Explore my work in Web development.</span> Each project reflects my skills and passion. Thanks for visiting!
       </p>
-      <small className="mb-2 block text-xs">
-        &copy; {currentYear} Manvendra Rajpoot. All rights reserved.
-      </small>
+
 
     </footer>
   );
