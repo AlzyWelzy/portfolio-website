bb642adca02df9e7fece675f8ab61a9b7e8731b2
Manvendra Rajpoot <welzyalzy@gmail.com>
2026-04-06 14:49:55 +0530

    Refactor resume GET route to use async file read

--- app/resume/route.ts
+++ app/resume/route.ts
@@ -1,24 +1,21 @@
 import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
-import { resumeName } from '@/lib/data';
+import { resumeName } from "@/lib/data";
 
 export async function GET() {
-    const filePath = path.join(process.cwd(), "public", `${resumeName}`);
+    const filePath = path.join(process.cwd(), "public", resumeName);
 
     try {
-        const fileBuffer = fs.readFileSync(filePath);
-        const response = new NextResponse(fileBuffer, {
+        const fileBuffer = await fs.promises.readFile(filePath);
+
+        return new NextResponse(fileBuffer, {
             headers: {
                 "Content-Type": "application/pdf",
-                "Content-Disposition": `attachment; filename="${resumeName}");"`,
+                "Content-Disposition": `attachment; filename="${resumeName}"`,
             },
-        })
-
-        return response;
+        });
     } catch (error) {
         return NextResponse.error();
     }
-
-
 }
